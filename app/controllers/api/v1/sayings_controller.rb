af0cb5a7026507c41f651054a6c369975150999f
jessebillard <jessebillard@mac.com>
2018-07-24 18:39:09 -0400

    allows for finding saying recording and sending data to frontend

--- app/controllers/api/v1/sayings_controller.rb
+++ app/controllers/api/v1/sayings_controller.rb
@@ -5,6 +5,13 @@ class Api::V1::SayingsController < ApplicationController
     end
 
     def show
+        # find the specific saying and render the recording attachment blob
+        # for now just return the selected saying until you figure out how to send the blob back
+        # byebug
+
+        @saying = Saying.find(params[:id])
+
+        send_data @saying.recording.download, content_type: @saying.recording.content_type
 
     end
 
@@ -12,7 +19,7 @@ class Api::V1::SayingsController < ApplicationController
         # byebug
         @saying = Saying.new(sayings_params)
         # byebug
-        @region = Region.find_by(name: params[:saying][:region])
+        @region = Region.find_by(name: params[:region])
         @saying.region = @region
 
         # just for now, make all sayings belong to the first and only user
@@ -24,6 +31,8 @@ class Api::V1::SayingsController < ApplicationController
         # take into account the votes...a new slang must have zero votes and must have votes value to save to db.
         @saying.votes = 0
 
+        @saying.recording.attach(params[:recording])
+        # byebug
         if @saying.save
             render json: @saying
         else
@@ -49,7 +58,7 @@ class Api::V1::SayingsController < ApplicationController
     private
 
     def sayings_params
-        params.require(:saying).permit(:title, :description)
+        params.permit(:title, :description)
     end
 
     def error_message

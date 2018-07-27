5c5b124a80e63f7eadceecf3a8e7b9f1161083e4
jessebillard <jessebillard@mac.com>
2018-07-27 11:58:23 -0400

    fixes and tests sayings and tags relationships

--- app/models/saying.rb
+++ app/models/saying.rb
@@ -1,6 +1,7 @@
 class Saying < ApplicationRecord
     belongs_to :user
     belongs_to :region
-    has_many :tags, through: :sayingtags
+    has_many :saying_tags
+    has_many :tags, through: :saying_tags
     has_one_attached :recording
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
-    has_many :sayings, through: :sayingtags
+    has_many :saying_tags
+    has_many :sayings, through: :saying_tags
 end
